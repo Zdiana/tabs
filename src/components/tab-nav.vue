aa77f8af79107bca8aea46fe2390a832c4cedf16
wb.zhangyanan01 <wb.zhangyanan01@mesg.corp.netease.com>
2018-10-17 11:37:50 +0800

    this is a test

---
+++ src/components/tab-nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ actionName }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        actionName: {
+            
+        }
+    }
+}
+</script>
+
---
+++ src/components/tab-pane.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="tab-pane">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    // tab-pane需要id标识唯一性  等下看看什么适合当作id穿过啦
+    props: {
+
+    }
+}
+</script>
