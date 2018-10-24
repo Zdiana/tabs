434522a77397ac308a4c8018f546bdba94c55703
wb.zhangyanan01 <wb.zhangyanan01@mesg.corp.netease.com>
2018-10-24 19:29:16 +0800

    我修改了feature_20181024中的标题

--- src/components/my-tab.vue
+++ src/components/my-tab.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h3>我改了tab标签的标题</h3>
+        <h3>我是feature_20181024分支的标题</h3>
         <tabs :class="[`is-${position}`]">
             <tab-pane label="用户管理" name="first" zyn="zhangzhang">item1</tab-pane>
             <!-- <tab-pane>item1</tab-pane>
